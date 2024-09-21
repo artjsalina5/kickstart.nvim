cf43fb20b75614ff42191e919dacf91538ec4357
artjsalina5 <artjsalina5@gmail.com>
2024-09-20 22:11:50 -0400

    sweepy

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,7 +3,7 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  { 'ThePrimeagen/vim-be-good' },
+
   {
     's1n7ax/nvim-window-picker',
     name = 'window-picker',
@@ -16,12 +16,24 @@ return {
   { 'xigoi/vim-arturo', lazy = true },
   { 'github/copilot.vim', lazy = false },
   {
+    'madox2/vim-ai',
+    config = function()
+      -- Set up your OpenAI API key
+      vim.g.vim_ai_token_file_path = '~/.config/openai.token'
+      -- Optional: Configure the plugin
+      vim.g.vim_ai_chat = {
+        ['options'] = {
+          ['model'] = 'gpt-4o',
+          ['temperature'] = 0.2,
+        },
+      }
+    end,
+  },
+  {
     'lervag/vimtex',
     lazy = false,
     config = function()
-      -- Set Zathura as the PDF viewer
       vim.opt.conceallevel = 1
-
       -- Any other VimTeX configurations you need
     end,
   },
